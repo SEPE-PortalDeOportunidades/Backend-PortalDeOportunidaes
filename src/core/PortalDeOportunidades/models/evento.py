42ec5c39b09b62002e34a7b55595c82227c16a3d
renato-cm <renatochagas.m@gmail.com>
2025-10-05 13:26:16 -0300

    feat: update Evento model to use DateTimeField for data and add local field

--- src/core/PortalDeOportunidades/models/evento.py
+++ src/core/PortalDeOportunidades/models/evento.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Evento(models.Model):
     descricao = models.CharField(max_length=250)
-    data = models.DateField()
+    data = models.DateTimeField(default=timezone.now)
     palestrante = models.CharField(max_length=200)
-
+    local = models.CharField(max_length=200, default='IFC')
+    
     def __str__(self):
-        return f"{self.descricao} - {self.palestrante}"
+        return f"{self.descricao} - {self.palestrante} - {self.local}"
